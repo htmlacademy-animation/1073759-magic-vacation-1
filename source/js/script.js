26cc8d6f3accca6641708049988687003a200321
Caterina <e.parshina@allfuneral.com>
2022-01-17 23:10:12 +0300

    adds body class animation

--- source/js/script.js
+++ source/js/script.js
@@ -21,3 +21,7 @@ social();
 
 const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
+
+window.addEventListener(`load`, function () {
+  document.body.classList.add(`loaded-page`);
+});
